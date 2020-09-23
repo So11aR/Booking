3347cb50105c72dda509f1927ed604a060879fee
Sergey <36004619+So11aR@users.noreply.github.com>
2020-09-23 12:11:49 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$('#btn-open-form').click(function(e){
+		e.preventDefault();
+
+		$(this).hide();
+		$('.booking-form-wrapper').slideDown(1000);
+	});
+
+	$(".phone-mask").mask("+7 (999) 999-99-99");
+
+
+
+	
+
+});
